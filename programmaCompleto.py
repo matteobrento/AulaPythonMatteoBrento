1c26d54f16c2d08ed6e50810280ca7929c1491cc
matteobrento <matteobrento.workaddress@gmail.com>
2024-06-21 14:53:10 +0200

    Add files via upload
    Aggiornamento con commenti, esercizio completo cicli

--- programmaCompleto.py
+++ programmaCompleto.py
@@ -6,19 +6,20 @@ while controllo:    #inizio del ciclo
     
     if(numPos > 0):    #condizione numero positivo
 
-        for i in range(1,numPos+1,1):
-            if(i%2 == 0):
+        for i in range(1,numPos+1,1):   #ciclo for che parte da 1 (lo 0 è escluso perchè permette di uscire dal programma)
+            if(i%2 == 0):               #e arriva al numero positivo inserito incluso, con step di 1, stampando la somma dei numeri pari
                 somma += i
-        print("La somma dei numeri pari è: ", somma)
+        print("La somma dei numeri pari è: ", somma)    #print della somma
         
-        for i in range(1,numPos+1,1):
-            if(i%2 != 0):
+        for i in range(1,numPos+1,1):   #ciclo for che parte da 1 (lo 0 è escluso perchè permette di uscire dal programma)
+            if(i%2 != 0):               #e arriva al numero positivo inserito incluso, con step di 1, stampando i numeri dispari
                 print(f"Numeri dispari: {i}")
 
-    elif(numPos == 0):
-        break
+    elif(numPos == 0):  #condizione di break, schiacciando 0 il programma termina
+        #break
+        controllo = False
         
     else:        
-        print("Il numero non è positivo ")
+        print("Il numero non è positivo ")  #numero non positivo, inserire di nuovo
         
 
