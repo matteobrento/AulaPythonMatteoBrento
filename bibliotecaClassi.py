b451eac5f6c452a1dfdd86bace1a82dc8448a20a
matteobrento <matteobrento.workaddress@gmail.com>
2024-07-01 16:34:42 +0200

    Add files via upload

--- bibliotecaClassi.py
+++ bibliotecaClassi.py
@@ -9,24 +9,24 @@ class Libro:
 
 class Biblioteca:
 
-    libri = []
+    libri = []  #creo una lista vuota
 
-    def aggiungi_libro(self):
+    def aggiungi_libro(self):   #chiedo in input gli attributi
         titolo = input("Inserisci il titolo: ")
         autore = input("Inserisci l'autore: ")
         pagine = int(input("Inserisci il numero di pagine: "))
-        libro = Libro(titolo, autore, pagine)
-        Biblioteca.libri.append(libro)
+        libro = Libro(titolo, autore, pagine)   #creo una variabile libro con all'interno l'oggetto Libro
+        Biblioteca.libri.append(libro)  #faccio un append del libro nella lista
         print("Libro aggiunto!")
 
     def mostra_libri(self):
-        if not Biblioteca.libri:
+        if not Biblioteca.libri:    #controllo se è vuota
             print("La biblioteca è vuota.")
         else:
-            for libro in Biblioteca.libri:
+            for libro in Biblioteca.libri:  #stampo il libro con il metodo descrizione() di libro
                 libro.descrizione()
 
-biblioteca = Biblioteca()
+biblioteca = Biblioteca()   #ciclo While per inserirne piu di uno
 while True:
     biblioteca.aggiungi_libro()
     biblioteca.mostra_libri()
