7bf7cef823a428d72c4e3da6cc12cdf03a44a2c2
matteobrento <matteobrento.workaddress@gmail.com>
2024-07-01 15:37:04 +0200

    Add files via upload
    commenti

--- libroClasse.py
+++ libroClasse.py
@@ -1,14 +1,14 @@
 class Libro:
 
-    def __init__(self, titolo, autore, pagine):
+    def __init__(self, titolo, autore, pagine): #definizione del costruttore con self e i 3 attributi
         self.titolo = titolo
         self.autore = autore
         self.pagine = pagine
 
-    def descrizione(self):
+    def descrizione(self):  #metodo di stampa degli attributi in una stringa
         print("Il libro",self.titolo,"è stato scritto da",self.autore,"e ha",self.pagine,"pagine")
 
 
-libro1 = Libro("Harry Potter", "Rowling", "300")
-libro1.descrizione()
+libro1 = Libro("Harry Potter", "Rowling", "300")    #creazione dell'oggetto libro1 di tipo Libro con i parametri passati
+libro1.descrizione()    #funzione di stampa chiamata
         
