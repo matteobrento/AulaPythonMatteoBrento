4d90b98e376bc982764cabf5531e53e8aaf8fba8
matteobrento <matteobrento.workaddress@gmail.com>
2024-07-01 15:31:35 +0200

    Add files via upload

---
+++ libroClasse.py
@@ -0,0 +1,14 @@
+class Libro:
+
+    def __init__(self, titolo, autore, pagine):
+        self.titolo = titolo
+        self.autore = autore
+        self.pagine = pagine
+
+    def stampa(self):
+        print("Il libro",self.titolo,"è stato scritto da",self.autore,"e ha",self.pagine,"pagine")
+
+
+libro1 = Libro("Harry Potter", "Rowling", "300")
+libro1.stampa()
+        
