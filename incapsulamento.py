59fc125a42103aaea24db14839e673bb67982688
matteobrento <matteobrento.workaddress@gmail.com>
2024-07-03 17:01:00 +0200

    Add files via upload
    Aggiornamento

--- incapsulamento.py
+++ incapsulamento.py
@@ -8,11 +8,18 @@ class ContoBancario:
     def set_saldo(self, saldo):
         self.__saldo = saldo
 
+    def get_titolare(self):
+        return self.__titolare
+    
+    def set_titolare(self, titolare):
+        self.__titolare = titolare
+
     def deposita(self):
         
         importo = int(input("Inserisci un importo che vuoi depositare: "))
         if importo > 0:
             ContoBancario.set_saldo = self.__saldo + importo
+            saldo_corrente = ContoBancario.set_saldo
             print(f"Deposito avvenuto con successo! Il tuo saldo è: {ContoBancario.set_saldo} euro")
         else:
             print("Importo negativo.")
@@ -30,7 +37,45 @@ class ContoBancario:
 
         print(f"Il tuo saldo è: {ContoBancario.set_saldo} euro")
 
+while True:
 
-conto = ContoBancario()
+    print("Menu: ")
+    print("1) Deposita")
+    print("2) Preleva")
+    print("3) Visualizza Saldo")
+    print("4) Visualizza Titolare")
+    print("5) Modifica Titolare\n")
+    cod = int(input("Inserisci l'opzione che vuoi effettuare: "))
+    if cod == 1:
+        conto = ContoBancario()
+        conto.deposita()
+    elif cod == 2:
+        conto = ContoBancario()
+        conto.preleva()
+    elif cod == 3:
+        conto = ContoBancario()
+        conto.visualizza_saldo()
+    elif cod == 4:
+        conto = ContoBancario()
+        print(f"Il titolare è: {conto.get_titolare()}")
+    elif cod == 5:
+        conto = ContoBancario()
+        conto.set_titolare("Luca")
+        print(f"Ora invece il titolare è: {conto.get_titolare()}")
+    else:
+        quit()
+
+    continua = input("Vuoi effettuare un'altra operazione ? ")
+    if continua != "si":
+        break
+
+
+
+""" conto = ContoBancario()
 conto.deposita()
-conto.preleva()
+print("\n")
+print(f"Il titolare è: {conto.get_titolare()}")
+conto.set_titolare("Luca")
+print(f"Ora invece il titolare è: {conto.get_titolare()}") """
+
+
