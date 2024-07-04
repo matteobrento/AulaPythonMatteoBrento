d31420b6da64855eeac10a5401a1ba203dbf33fe
matteobrento <matteobrento.workaddress@gmail.com>
2024-07-04 15:36:48 +0200

    Add files via upload
    Versione aggiornata

--- es1Polimorfismo.py
+++ es1Polimorfismo.py
@@ -31,8 +31,34 @@ class GestorePagamenti(MetodoPagamento):
         metodo.effettua_pagamento()
 
 
+while True:
+    print("Menu")
+    print("Scegli il metodo di pagamento: 1) Carta di Credito, 2) PayPal, 3) Bonifico Bancario ")
+    cod = int(input("Scegli un opzione di pagamento: "))
+    print("\n")
+    if cod == 1:
+        carta = CartaDiCredito()
+        gestore = GestorePagamenti()
+        gestore.pagamento(carta)
+    elif cod == 2:
+        pay = PayPal()
+        gestore = GestorePagamenti()
+        gestore.pagamento(pay)
+    elif cod == 3:
+        bonifico = BonificoBancario()
+        gestore = GestorePagamenti()
+        gestore.pagamento(bonifico)
+    else:
+        print("Hai selezionato un'opzione non supportata. ")
+
+    print("\n")
+    continua = input("Desideri effettuare un altro pagamento ? ")
+    if continua.lower() != "si":
+        print("Arrivederci!")
+        break
 
 
-carta = CartaDiCredito()
-gestore = GestorePagamenti()
-gestore.pagamento(carta)
+
+
+
+
