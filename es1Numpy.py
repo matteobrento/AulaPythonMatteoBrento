a336456de82ecec19241ee10259d71798735031a
matteobrento <matteobrento.workaddress@gmail.com>
2024-07-15 16:09:06 +0200

    Add files via upload
    Aggiornamento

--- es1Numpy.py
+++ es1Numpy.py
@@ -9,10 +9,55 @@ Stampa la forma dell'array.
 
 import numpy as np
 
-array = np.arange(10,49)
-print(array.dtype)
-print(array.shape)
+controllo = True
 
-array = np.arange(10,49,dtype="float")
-print(array.dtype)
-print(array.shape)
+def crea_array():
+    int1 = int(input("Inserisci un valore: "))
+    int2 = int(input("Inserisci un valore: "))
+    s = "int64"
+    s1 = "float64"
+    scelta = int(input("1) dType INT, 2)dType FLOAT: "))
+    if scelta == 1:
+        array = np.arange(int1,int2,dtype=s)
+        print(array)
+    elif scelta == 2:
+        array = np.arange(int1,int2,dtype=s1)
+        print(array)
+    else:
+        print("Opzione non valida!")
+
+    return array
+
+def tipo():
+
+    array = crea_array()
+    print(array.dtype)
+
+def struttura():
+
+    array = crea_array()
+    print(array.shape)
+
+
+while controllo:
+
+    print("Benvenuto!")
+    print("1)Crea Array ")
+    print("2)Visualizza Tipo Array ")
+    print("3)Visualizza Struttura Array ")
+    opzione = int(input("\nScegli l'operazione che desideri effettuare: "))
+
+    if opzione == 1:
+        crea_array()
+    elif opzione == 2:
+        tipo()
+    elif opzione == 3:
+        struttura()
+    else:
+        print("Opzione non valida!")
+        quit()
+
+    scelta = input("\nVuoi effettuare un altra operazione ? ")
+    if scelta.lower() != "si":
+        print("\nSistema in chiusura. Arrivederci! ")
+        break
