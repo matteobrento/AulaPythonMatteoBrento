ad0591ee97d671e0150f9501afbc26ad1a377772
matteobrento <matteobrento.workaddress@gmail.com>
2024-07-16 13:36:56 +0200

    Delete AcademyItConsulting/18.06 directory

--- AcademyItConsulting/18.06/es3.py
+++
@@ -1,5 +0,0 @@
-#Creare un if con else semplice, dentro l'if inserire una struttura di creazione dati(nome, password, id del sistema a crescere) e
-#nell'else il controllo automatico la dove è presente l'account del sistema e solo se si passa dall'else
-
-id = 0
-
