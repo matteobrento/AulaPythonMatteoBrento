77054dd23da1e4a7ca3b15256641ffff322ebfa0
matteobrento <matteobrento.workaddress@gmail.com>
2024-07-17 01:07:26 +0200

    Add files via upload
    Aggiornamento esercizio completo con Daniele e Danilo

--- AcademyItConsulting/Esercitazioni/16.07/es_completo.py
+++ AcademyItConsulting/Esercitazioni/16.07/es_completo.py
@@ -1,39 +1,50 @@
-import parteuno as mod1
+import parteuno as mod1     #import modulo con funzioni
 
+#menu
 def menu():
-    print("""\n1. Stampa matrice
+    print("""\nMenù:
+1. Stampa matrice
 2. Estrai sottomatrice
 3. Matrice trasposta        
 4. Somma elementi matrice
 5. Moltiplicazione element-wise
 6. Media elementi matrice
 7. Calcolo determinante                     
-0. Esci\n
+0. Esci
 """)
-    scelta_menu=input("Inserisci una scelta: ")
+    scelta_menu=input("\nScegli un opzione: ")
     return scelta_menu
 
+#main
 def main():
-    mat,righe,col=mod1.crea_matrice()
+    print("\nCreazione Matrice 2D...\n")
+    mat,righe,col=mod1.crea_matrice()   #creazione della matrice di partenza fuori dal while
+
     while True:
         scelta=menu()
-        if scelta=="1":
-            print(f"\nLa matrice è:\n {mat}")
-        elif scelta=="2":
+        if scelta=="1":         #Stampa matrice
+            print(f"\nLa matrice 2D di partenza è:\n {mat}")
+        elif scelta=="2":       #Estrai sottomatrice
             mod1.sotto_matrice(mat,righe,col)
-        elif scelta=="3":
+        elif scelta=="3":       #Matrice trasposta
             mod1.trasp_matrice(mat)
-        elif scelta=="4":
+        elif scelta=="4":       #Somma elementi matrice
             mod1.somma_elem(mat)
-        elif scelta=="5":
+        elif scelta=="5":       #Moltiplicazione element-wise
             mod1.molt_elem_wise(mat,righe,col)
-        elif scelta=="6":
+        elif scelta=="6":       #Media elementi matrice
             mod1.media_mat(mat)
-        elif scelta=="7":
+        elif scelta=="7":       #Calcolo determinante
             mod1.det_mat(mat,righe,col)
-        elif scelta=="0":
+        elif scelta=="0":       #Esci
             break
         else:
             print("Scelta sbagliata")
 
-main()
+        nuova_scelta = input("\nVuoi effettuare un'altra operazione? ") #ripetibilità gestita tramite la richiesta di una nuova operazione
+        if nuova_scelta.lower() != "si":
+            print("\nSistema in chiusura. Arrivederci!")
+            break
+
+
+main()      #richiamo al main
--- AcademyItConsulting/Esercitazioni/16.07/parteuno.py
+++ AcademyItConsulting/Esercitazioni/16.07/parteuno.py
@@ -1,58 +1,64 @@
 """
-Parte UNO: Scrivere un Sistema che utilizza NumPy per gestire una matrice 2D. 
-
+Parte UNO: 
+Scrivere un Sistema che utilizza NumPy per gestire una matrice 2D. 
 Il programma deve presentare un menu interattivo che consente all'utente
 di eseguire varie operazioni sulla matrice. Le operazioni disponibili includono:
-
 Creare una nuova matrice 2D di dimensioni specificate con numeri casuali.
 Estrarre e stampare la sotto-matrice centrale.
 Trasporre la matrice e stamparla.
 Calcolare e stampare la somma di tutti gli elementi della matrice.
 Uscire dal programma.
 """
+
 import numpy as np
 
+#creazione della matrice
 def crea_matrice():
-    while True:
-        try:
+    while True:     #inserimento righe e colonne con un while finchè non viene inserito un valore accettabile
+        try:        
             righe=int(input("Inserisci il numero di righe: "))
             break
-        except:
-            print("Errore. Riprova")
+        except ValueError as e:
+            print("\nErrore nel valore passato:", e, "\nInserisci un valore intero valido! \n")   #except con ValueError nel caso in cui si inserisce un valore non intero
     while True:
         try:
             col=int(input("Inserisci il numero di colonne: "))
             break
-        except:
-            print("Errore. Riprova")
+        except ValueError as e:
+            print("\nErrore nel valore passato:", e, "\nInserisci un valore intero valido! \n")   #except con ValueError nel caso in cui si inserisce un valore non intero
 
     matrice = np.random.randint(0,100,size=(righe,col))
-    print(f"La matrice è:\n {matrice}")
+    print(f"\nLa matrice 2D di partenza è:\n {matrice}")
 
-    return matrice,righe,col
+    return matrice,righe,col    #return della matrice creata, delle righe e delle colonne
 
+#calcolo sottomatrice dalla prima riga alla penultima, e dalla prima colonna alla penultima
 def sotto_matrice(matrice,righe,col):
-    print(f"Sotto matrice centrale:\n {matrice[1:righe-1,1:col-1]}")
+    print(f"\nSotto-Matrice centrale:\n {matrice[1:righe-1,1:col-1]}")    #sotto-matrice tramite Slicing
 
+#calcolo matrice trasposta
 def trasp_matrice(matrice):
-    matr=matrice.T
-    print(matr)
+    trasposta=matrice.T     #metodo numpy per il calcolo della trasposta
+    print("\nMatrice Trasposta: \n", trasposta)
 
+#somma degli elementi della matrice
 def somma_elem(matrice):
-    print(f"La somma della matrice è: {np.sum(matrice)}")
+    print(f"\nLa somma degli elementi della matrice è: {np.sum(matrice)}")
 
+#calcolo determinante della matrice se righe==col
 def det_mat(matrice,righe,col):
     if righe==col:
-        print(f"Il determinante è: {np.linalg.det(matrice)}")
+        print(f"\nIl determinante della matrice è: {np.linalg.det(matrice)}") #calcolo determinante tramite linalg.det solo se righe e colonne sono uguali
     else:
-        print("Righe e colonne diverse. Non si può calcolare il determinante")
+        print("\nNumero di righe e colonne diverso. Non si può calcolare il determinante. ")
 
-def molt_elem_wise(mat,righe,col):
-    mat2=np.random.randint(0,100,size=(righe,col))
-    #moltiplicazione=np.dot(mat,mat2)
-    molt=mat*mat2
-    print(f"Seconda matrice generata:\n{mat2}")
-    print(f"Moltiplicazione delle due matrici:\n {molt}")
+#moltiplicazione element-wise
+def molt_elem_wise(matrice,righe,col):
+    matrice2=np.random.randint(0,100,size=(righe,col))
+    prodotto_matrici=matrice*matrice2   #prodotto elemento per elemento
+    print(f"\nSeconda matrice generata:\n{matrice2}\n")
+    print(f"Prodotto delle due matrici:\n {prodotto_matrici}")
 
+#calcolo media della matricd
 def media_mat(matrice):
-    print(f"La media della matrice è: {np.mean(matrice)}")
+    print(f"\nLa media degli elementi della matrice è: {np.mean(matrice)}")
