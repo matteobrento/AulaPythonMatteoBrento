67b57455ebe9cdf50a865cd9505a0c8439a46719
matteobrento <matteobrento.workaddress@gmail.com>
2024-07-16 15:28:14 +0200

    Add files via upload
    Aggiornamento recap 1

--- AcademyItConsulting/Esercitazioni/16.07/recap1.py
+++ AcademyItConsulting/Esercitazioni/16.07/recap1.py
@@ -5,7 +5,7 @@ print("Array: ", array, "\n")
 print("La somma degli elementi dell'array è: ", array.sum(), "\n")
 print("La media degli elementi dell'array è: ", array.mean()) """
 
-array = np.random.randint(0,100,15)
+array = np.random.randint(0,100,15) #array di 15 valori casuali tra 0 e 100
 print("Array: ", array, "\n")
 
 while True:
@@ -15,13 +15,13 @@ while True:
     print("2. Media\n")
     scelta = int(input("Scegli un opzione: "))
     if scelta == 1:
-        print("La somma degli elementi dell'array è: ", array.sum(), "\n")
+        print("La somma degli elementi dell'array è: ", array.sum(), "\n")  #somma con array.sum()
     elif scelta == 2:
-        print("La media degli elementi dell'array è: ", array.mean(), "\n")
+        print("La media degli elementi dell'array è: ", array.mean(), "\n") #media con array.mean()
     else:
         print("Opzione non disponibile!")
 
-    tent = input("Vuoi eseguire un'altra operazione ? ")
+    tent = input("Vuoi eseguire un'altra operazione ? ")    #ripetibilità
     if tent != "si":
         print("\nArrivederci")
         break
